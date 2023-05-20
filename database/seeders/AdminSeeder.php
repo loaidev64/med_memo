b5a5615849184dcf2510e0543aef15b568c61ef7
Loai Judeh <121749595+loaidev64@users.noreply.github.com>
2023-05-20 12:26:27 +0300

    Laravel: Role, seeders, edit stubs, make dynamic auto required routes (#4)

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'role_id' => 1,
+        ])->create();
+    }
+}
